8c33989e6a5d246ab337755e6eed01eca7cb7df4
sobiamustafa95 <sobiamustafa95@gmail.com>
2022-01-19 18:44:03 +0500

    code updated

--- src/Features/HomeScreen/index.js
+++ src/Features/HomeScreen/index.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+// import AppStack from '../../Navigation/AppStack';
 
 const HomeScreen = () => {
 
   return (
     <View  style = {styles.container}>
-      <Text style = {styles.paragraph}>Home Screen</Text>
+      <Text>Home Screen</Text>
+      {/* <AppStack /> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  }
 });
 
 export default HomeScreen;
