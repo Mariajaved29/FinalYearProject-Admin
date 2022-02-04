d69ef55d1ddc6da32cc99d9ab897ff0a46547969
sobiamustafa95 <sobiamustafa95@gmail.com>
2022-02-04 22:46:11 +0500

    setting screen added

--- src/Features/SettingsScreen/index.js
+++ src/Features/SettingsScreen/index.js
@@ -1,12 +1,164 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// import React, {Component} from 'react';
+// import FontAwesome from 'react-native-vector-icons/FontAwesome'
+// import {
+//   View,
+//   Text,
+//   ScrollView,
+//   StyleSheet,
+//   Image,
+//   TouchableOpacity,
+//   SafeAreaView ,
+// } from 'react-native';
 
-const SettingsScreen = () => {
-  return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Text>Settings Screen</Text>
-    </View>
-  )
-}
+// const SettingsScreen = () => {
 
-export default SettingsScreen
+//   return (
+  
+//     <SafeAreaView style={styles.container} >
+//      <View style={styles.bannerContainer}>
+//             <Image
+//               source={require('../../assets/01.jpg')}
+//               style={styles.bannerImage}
+//             ></Image>
+//             <View style={styles.bannerShade} />
+           
+//           </View>
+//     <ScrollView >
+//     <View style={{marginTop:120}}>
+   
+                
+//     <View style={styles.cards} >
+  
+//     <View style={{flexDirection:'row',flex:1,justifyContent:'space-between'}}>
+    
+
+// <View >
+// <Text style={styles.heading }><FontAwesome name='user-o' size={14} style={{}} />   Account</Text>
+// </View>
+// <TouchableOpacity>
+//     <View>
+//  <FontAwesome name='arrow-right' size={14} style={{marginTop:15}} />
+//  </View>
+//  </TouchableOpacity>
+// </View>
+// </View>
+
+//  <View style={styles.cards} >
+//     <View style={{flexDirection:'row',flex:1,justifyContent:'space-between'}}>
+    
+// <Text style={styles.heading}><FontAwesome name='bell' size={14} style={{}} />   Notification</Text>
+
+// <TouchableOpacity>
+//  <FontAwesome name='arrow-right' size={14} style={{marginTop:15}} />
+//  </TouchableOpacity>
+// </View>
+// </View>
+
+//  <View style={styles.cards} >
+//     <View style={{flexDirection:'row',flex:1,justifyContent:'space-between'}}>
+    
+// <Text style={styles.heading}><FontAwesome name='file' size={14} style={{}} />   Privacy Security</Text>
+
+//  <TouchableOpacity>
+//  <FontAwesome name='arrow-right' size={14} style={{marginTop:15}} />
+//  </TouchableOpacity>
+// </View>
+// </View>
+
+//  <View style={styles.cards} >
+//     <View style={{flexDirection:'row',flex:1,justifyContent:'space-between'}}>
+    
+// <Text style={styles.heading}><FontAwesome name='info' size={14} style={{}} />   About</Text>
+
+//  <TouchableOpacity>
+//  <FontAwesome name='arrow-right' size={14} style={{marginTop:15}} />
+//  </TouchableOpacity>
+// </View>
+// </View>
+
+
+//  <View style={styles.cards} >
+//     <View style={{flexDirection:'row',flex:1,justifyContent:'space-between'}}>
+    
+// <Text style={styles.heading}> <FontAwesome name='question-circle' size={14} style={{}} />  Help and Support</Text>
+
+//  <TouchableOpacity>
+//  <FontAwesome name='arrow-right' size={14} style={{marginTop:15}} />
+//  </TouchableOpacity>
+// </View>
+// </View>
+
+
+// </View>
+// </ScrollView>
+//     </SafeAreaView>
+
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+    
+    
+    
+   
+//   },
+ 
+// cards:{
+ 
+//   flex:1,
+    
+//   height:60,
+//   width:"95%",
+//   backgroundColor:"#FFFFFF",
+//   borderRadius:0,
+//   padding:10,
+//   elevation:10,
+//   shadowColor: '#000',
+//   shadowOffset: { width: 0, height: 3 },
+//   shadowOpacity: 1,
+//   shadowRadius: 5,
+//   borderWidth:1,borderColor:'#ededed',
+//   margin:10,
+  
+// },
+// heading:{
+//   fontSize:16,
+//   fontWeight: "bold",
+//   color:'#000000',
+//   marginTop:10,
+//   marginLeft: 10,
+ 
+ 
+// },
+
+
+
+
+// bannerContainer: {height: 100, width: '100%'},
+// bannerImage: {
+//   width: '100%',
+//   height: 200,
+//   // borderWidth: 2,
+//   borderBottomRightRadius: 50,
+//   borderBottomLeftRadius: 50,
+//   overflow: 'hidden',
+//   resizeMode: 'cover',
+// },
+// bannerShade: {
+//   height: 200,
+//   width: '100%',
+//   position: 'absolute',
+//   left: '0%',
+//   top: '0%',
+//   backgroundColor: 'black',
+//   opacity: 0.3,
+//   borderBottomRightRadius: 50,
+//   borderBottomLeftRadius: 50,
+//   overflow: 'hidden',
+ 
+// },
+// });
+
+// export default SettingsScreen;
+
